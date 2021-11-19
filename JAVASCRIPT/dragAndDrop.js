f9dddbc67393ee9748de34c67bf3325bd649c14d
Mahdieh <mahdeh.abed@gmail.com>
2021-11-19 06:20:20 +0330

    third commit

--- JAVASCRIPT/dragAndDrop.js
+++ JAVASCRIPT/dragAndDrop.js
@@ -1,13 +1,37 @@
-function allowDrop(ev) {
-    ev.preventDefault();
+const range = document.getElementById("list__todo")
+dragElement(range)
+
+function dragElement(element) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(element.id + "Drag")) {
+        document.getElementById(element.id + "Drag").onmousedown = dragMouseDown;
+    } else {
+        element.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(a) {
+        a = a || window.addEventListener("mousedown", this.window);
+        a.preventDefault();
+        pos3 = a.clientX;
+        pos4 = a.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(b) {
+        b = b || window.addEventListener("mousedown", this.window);
+        b.preventDefault();
+        pos1 = pos3 - b.clientX;
+        pos2 = pos4 - b.clientY;
+        pos3 = b.clientX;
+        pos4 = b.clientY;
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    let data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
